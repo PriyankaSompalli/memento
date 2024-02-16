2b4dab578b2521cf873b62c31813dfea97fd5a7e
PriyankaSompalli <117505463+PriyankaSompalli@users.noreply.github.com>
2024-02-16 21:29:59 +0530

    Add files via upload

---
+++ mermto.js
@@ -0,0 +1,7 @@
+function searchProducts() {
+    var searchTerm = document.getElementById('searchInput').value;
+    // Code to search for products using the searchTerm
+    // This is just a placeholder function
+    alert('Searching for: ' + searchTerm);
+  }
+  
